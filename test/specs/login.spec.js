e5335d1dbeaef49c412a3ffd0e8510042a0a9340
argabas <argabas@outlook.com>
2023-06-22 16:51:53 -0300

    versao final aula 16

---
+++ test/specs/login.spec.js
@@ -0,0 +1,8 @@
+const homeScreen = require("../screens/home.screen");
+
+describe('Access Admin Panel', () => {
+    it('shoud login with valid credentials', async () => {
+        await homeScreen.goToLogin()
+    });
+    
+})
